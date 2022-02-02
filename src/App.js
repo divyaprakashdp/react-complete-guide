3359e54dcff9e34f46fc2d74fdb38ab259a8272a
divya prakash <d.prakash1506@gmail.com>
2022-02-02 23:39:57 +0530

    created expenseDate component & updated ugly date

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import ExpenseItems from "./components/ExpenseItems";
 function App() {
   const expenses = [
     { title: "Car Insurance", amount: 295.3, date: new Date(2022, 1, 22) },
-    { title: "Party", amount: 150.5, date: new Date(2022, 1, 26) },
-    { title: "Toilet Paper", amount: 100.3, date: new Date(2022, 1, 29) },
+    { title: "Party", amount: 150.5, date: new Date(2022, 7, 26) },
+    { title: "Toilet Paper", amount: 100.3, date: new Date(2022, 0, 29) },
   ];
   return (
     <div>
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,10 +1,13 @@
 import './ExpenseItem.css'
+import './ExpenseDate.js'
+import ExpenseDate from './ExpenseDate.js';
 
 function ExpenseItems(props) {
     
+
   return (
     <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date}/>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
