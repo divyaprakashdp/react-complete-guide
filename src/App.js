73d25a52c36ace00a804a980bdd0aca913eeaefa
divya prakash <d.prakash1506@gmail.com>
2022-02-03 10:52:23 +0530

    Added Card.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import ExpenseItems from "./components/ExpenseItems";
-
+import Expenses from "./components/Expenses";
 function App() {
   const expenses = [
     { title: "Car Insurance", amount: 295.3, date: new Date(2022, 1, 22) },
@@ -8,22 +7,8 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started! YES ha</h2>
-      <ExpenseItems
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItems>
-      <ExpenseItems
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItems>
-      <ExpenseItems
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItems>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
