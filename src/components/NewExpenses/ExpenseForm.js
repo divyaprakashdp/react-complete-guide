d2668fd40c82471c1e22979fa4bebf6ca8f53562
divya prakash <d.prakash1506@gmail.com>
2022-02-04 10:19:53 +0530

    Added state handling

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,22 +1,65 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+
+    //alternate
+    // const [enteredInput, setEnteredInput] = useState({
+    //   title:'',
+    //   date:'',
+    //   amount:''
+    // });
+
+
+    const titleChangeHandler = (event) => {
+      setEnteredTitle(event.target.value);
+      //alternate#1
+      // setEnteredInput({
+      //   ...enteredInput,
+      //   title: event.target.value
+      // })
+
+      //alternate#2
+      // setEnteredInput((prevState) => {
+      //     return {...prevState, title:event.target.value};
+      // });
+    }
+
+    const dateChangeHandler = (event) =>{
+      setEnteredDate(event.target.value);
+      // setEnteredInput({
+      //   ...enteredInput,
+      //   date: event.target.value
+      // })
+    }
+
+    const amountChangeHandler = (event) =>{
+      setEnteredAmount(event.target.value);
+      // setEnteredInput({
+      //   ...enteredInput,
+      //   amount: event.target.value
+      // })
+    }
+    
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler}/>
         </div>
 
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" />
+          <input type="number" onChange={dateChangeHandler}/>
         </div>
 
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" />
+          <input type="date" onChange={amountChangeHandler}/>
         </div>
       </div>
 
