a8ebc6fdbe8cbe6874c0f4ff52ed33f2ccaef2ca
divya prakash <d.prakash1506@gmail.com>
2022-02-08 23:43:01 +0530

    Added new expense functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import React, { useState } from 'react';
 import Expenses from "./components/Expenses/Expenses.js";
 import NewExpense from "./components/NewExpenses/NewExpense.js";
-function App() {
-  const expenses = [
-    { title: "Car Insurance", amount: 295.3, date: new Date(2022, 1, 22) },
-    { title: "Party", amount: 150.5, date: new Date(2022, 7, 26) },
-    { title: "Toilet Paper", amount: 100.3, date: new Date(2022, 0, 29) },
-  ];
 
-  const addExpenseHandler = (expense) =>{
-      console.log(expense);
-      
-  }
+const dummyExpenses = [
+  { title: "Car Insurance", amount: 295.3, date: new Date(2022, 1, 22) },
+  { title: "Party", amount: 150.5, date: new Date(2022, 7, 26) },
+  { title: "Toilet Paper", amount: 100.3, date: new Date(2022, 0, 29) },
+];
+
+
+function App() {
+      const [expenses, setNewExpense] = useState(dummyExpenses);
+      const addExpenseHandler = (expense) =>{
+        setNewExpense((prevExpenses) =>{
+          return [expense, ...prevExpenses]
+        });
+        // console.log(expenses)
+      }
   return (
     
     <div>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,21 +14,7 @@ const Expenses=(props) =>{
   return (
     <Card className="expenses">
       <ExpensesFilter selected={selectedFilterYear} onFilterChange={filterChangeHandler}/>
-      <ExpenseItems
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      ></ExpenseItems>
-      <ExpenseItems
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      ></ExpenseItems>
-      <ExpenseItems
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      ></ExpenseItems>
+      {props.items.map(expense => <ExpenseItems title={expense.title} amount={expense.amount} date={expense.date}/>)}
     </Card>
   );
 }
