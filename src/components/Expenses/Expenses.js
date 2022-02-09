8579233781d095910706f940f4a152838b96a3da
divya prakash <d.prakash1506@gmail.com>
2022-02-09 10:10:33 +0530

    Separated expense list with conditional return

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import ExpenseItems from "./ExpenseItems";
 import Card from "../Card/Card.js";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpenseFilter";
+import ExpenseList from "./ExpenseList";
 
 const Expenses = (props) => {
   const [selectedFilterYear, setFilterYear] = useState("2022");
@@ -10,7 +10,7 @@ const Expenses = (props) => {
     setFilterYear(filterYear);
     // console.log(selectedFilterYear);
   };
-  const filteredExpenses = props.items.filter(expense =>{
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === selectedFilterYear;
   });
 
@@ -20,14 +20,7 @@ const Expenses = (props) => {
         selected={selectedFilterYear}
         onFilterChange={filterChangeHandler}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItems
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      <ExpenseList items={filteredExpenses} />
     </Card>
   );
 };
