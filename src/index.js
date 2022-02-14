532ecbd263e3dd735242905a27e2eb2ef559fb7f
divya prakash <d.prakash1506@gmail.com>
2022-02-14 14:51:07 +0530

    Added Add-user

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 // import App from './Expense-Monitor/App'; //uncomment to see expense app
-import App from './CourseGoal/App';
+// import App from './CourseGoal/App';//uncomment to see
+import App from './Add-User/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
