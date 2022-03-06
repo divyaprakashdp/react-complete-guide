5431753fbcf9f6e2fbbbbda994f7dc8db5520031
divya prakash <d.prakash1506@gmail.com>
2022-03-06 13:03:08 +0530

    Updated adduser with REact.fragment

--- src/Add-User/components/Users/AddUsers.js
+++ src/Add-User/components/Users/AddUsers.js
@@ -46,7 +46,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <React.Fragment>
       {error && (
         <ErrorModal
           title={error.title}
@@ -75,7 +75,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </React.Fragment>
   );
 };
 
